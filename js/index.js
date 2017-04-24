85fde4a5b9013783bd7f2f92d648e39c89b0f63d
Mayra <mayracallves@gmail.com>
2017-04-24 15:53:03 -0300

    Centralizamos depoimentos e alteramos css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
+$('#magic-button-1').click(function(){
+  $('#magic-button-1').fadeOut(300);
+  $('#success-message-1').fadeIn(3000);
+});
+
+$('#magic-button-2').click(function(){
+  $('#magic-button-2').fadeOut(300);
+  $('#success-message-2').fadeIn(3000);
+});
+
+$('#magic-button-3').click(function(){
+  $('#magic-button-3').fadeOut(300);
+  $('#success-message-3').fadeIn(3000);
+});
+
+$('#magic-button-4').click(function(){
+  $('#magic-button-4').fadeOut(300);
+  $('#success-message-4').fadeIn(3000);
+});
